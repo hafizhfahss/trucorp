ee50015b7891ffd9b296360d73c89bb1d8e874c1
hafizhfahss <hafizh.fahsa80@gmail.com>
2021-02-17 17:56:21 -0500

    update git trucorp

--- trucorp-web/index.php
+++ trucorp-web/index.php
@@ -4,18 +4,30 @@
 	<title>Trucorp.web</title>
 </head>
 <body>
+
+	
+
 	<?php 
 		$conn = mysqli_connect('172.18.0.2','root','owlowl','Trucorp');
 		$query = "SELECT *FROM users";
 		$result = $conn->query($query);
+		$user = 0;
+
+		<tr>
+			<th>ID</th>
+			<th>Nama</th>
+			<th>Kantor</th>		
+		</tr>		
+
 		while($data=$result->fetch_assoc())
 		{?>
+		<tr>
+			<td><?php echo $data['id']?></td>
+			<td><?php echo $data['nama']?></td>
+			<td><?php echo $data['kantor']?></td>
+		</tr>
 
-			<?php echo $data['id']?>
-			<?php echo $data['nama']?>
-			<?php echo $data['kantor']?>
-
-		<?php }
-	 ?>
+		<?php $user = $user + 1; }
+		echo "Jumlah User Terdaftar = ".$user;?>
 </body>
 </html>
