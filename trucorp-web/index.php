4a315a69f92993a7fb9046d334797f3e8e92a007
hafizhfahss <61222459+hafizhfahss@users.noreply.github.com>
2021-02-17 07:10:00 -0500

    Update index.php

--- trucorp-web/index.php
+++ trucorp-web/index.php
@@ -5,14 +5,13 @@
 </head>
 <body>
 
-<<<<<<< HEAD
 	<table border=1>
 		<tr>
                         <th>ID</th>
                         <th>Nama</th>
                         <th>Kantor</th>         
                 </tr> 
-=======
+
 	
 	<tr>
 		<th>ID</th>
@@ -20,17 +19,16 @@
 		<th>Kantor</th>		
 	</tr>	
 	
->>>>>>> e2f5fdfb1ca2a52706d9e58b12c883010cb6e8d3
+
 
 	<?php 
 		$conn = mysqli_connect('172.21.0.3','root','owlowl','Trucorp');
 		$query = "SELECT *FROM users";
 		$result = $conn->query($query);
 		$user = 0;
-<<<<<<< HEAD
-=======
+
 	
->>>>>>> e2f5fdfb1ca2a52706d9e58b12c883010cb6e8d3
+
 
 		while($data=$result->fetch_assoc())
 		{?>
